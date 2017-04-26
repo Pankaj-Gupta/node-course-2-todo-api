7199f821b4ad28e253b511c5cae04919997aeaf4
Pankaj Gupta <guptapankaj1993@gmail.com>
2017-04-26 19:32:24 +0530

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://guptapankaj1993:gupta1993@ds121091.mlab.com:21091/nodejscourse' || 'mongodb://localhost:27017/TodoApp')
 
 module.exports = {mongoose};
