29ad6d6c716223b46c7250aea922a9404d9a1ddd
Pankaj Gupta <guptapankaj1993@gmail.com>
2017-04-28 20:48:11 +0530

    Add POST /users route and token method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://guptapankaj1993:gupta1993@ds121091.mlab.com:21091/nodejscourse' || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://guptapankaj1993:gupta1993@ds121091.mlab.com:21091/nodejscourse')
 
 module.exports = {mongoose};
